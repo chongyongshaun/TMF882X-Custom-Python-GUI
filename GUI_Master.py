fc13474c2b69b7396ecaa2a06fc46c0e77651590
chongyongshaun <chongyongshaun@gmail.com>
2025-06-13 15:01:35 +0100

    add baud rate and COM port option menu gui

--- GUI_Master.py
+++ GUI_Master.py
@@ -13,14 +13,52 @@ class ComGUI:
         self.frame = LabelFrame(root, text="COM Manager", padx=5, pady=5, bg="white")
         self.label_com = Label(self.frame, text="Available Ports:", bg="white", width=15, anchor="w")
         self.label_bd = Label(self.frame, text="Baud Rate:", bg="white", width=15, anchor="w")
+        self.ComOptionMenu()
+        self.baudOptionMenu()
 
-
+        self.padx = 20
+        self.pady = 5
         self.publish()
+
+    def ComOptionMenu(self):
+        coms = ["-", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9"]
+        self.clicked_com = StringVar()
+        self.clicked_com.set(coms[0])
+        self.drop_com = OptionMenu(self.frame, self.clicked_com, *coms)
+        self.drop_com.config(width=10)
+
+    def baudOptionMenu(self):
+        '''
+         Method to list all the baud rates in a drop menu
+        '''
+        self.clicked_bd = StringVar()
+        bds = ["-",
+               "300",
+               "600",
+               "1200",
+               "2400",
+               "4800",
+               "9600",
+               "14400",
+               "19200",
+               "28800",
+               "38400",
+               "56000",
+               "57600",
+               "115200",
+               "128000",
+               "256000"]
+        self.clicked_bd .set(bds[0])
+        self.drop_baud = OptionMenu(
+            self.frame, self.clicked_bd, *bds)
+        self.drop_baud.config(width=10)
 
     def publish(self):
         self.frame.grid(row=0, column=0, padx=5, pady=5, columnspan=3, rowspan=3)
         self.label_com.grid(row=2, column=1)
+        self.drop_com.grid(row=2, column=2)
         self.label_bd.grid(row=3, column=1)
+        self.drop_baud.grid(row=3, column=2)
 
 
 if __name__ == "__main__": #make sure this only runs when this file is run directly
